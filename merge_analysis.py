efb1bf4a015763d07ed7f5edbdc91a75ea8f3636
tayanemoura <tayanemoura@id.uff.br>
2018-05-07 23:24:11 -0300

    create function to return dict with metrics

--- merge_analysis.py
+++ merge_analysis.py
@@ -1,6 +1,7 @@
 from pygit2 import *
 import os
 import sys
+import shutil
 
 
 class Action(object):
@@ -24,7 +25,6 @@ def print_actions(list_actions):
 
 
 def actions_to_set(list_actions):
-	#set_actions = {}
 	set_actions = set()
 
 	for action in list_actions:
@@ -32,39 +32,89 @@ def actions_to_set(list_actions):
 
 		set_actions.add(aux)
 
-	print (set_actions)
 	return set_actions
 
 
 def set_actions(diff_a_b):
 	list_actions = []
-	diff_a_b.find_similar()
+	
 	for d in diff_a_b:
-		#here gets the similiarity percentage
+		file_name = d.delta.new_file.path
 		
-		#print (d.delta.similarity)
-		if d.delta.similarity == 100:
-			#print(d.delta.new_file.path)
-			#print(d.delta.old_file.path)
-			action = Action(d.delta.old_file.path, d.delta.new_file.path, "renamed_file", -2, -2)
-			list_actions.append(action)
-
-		else:
-			#print (d.patch)
-			file_name = d.delta.new_file.path
-			for h in d.hunks:
-				#print (h)
-				#print (h.header)
-				for l in h.lines:
-					action = Action(l.content, file_name, l.origin, l.new_lineno, l.old_lineno)
-					list_actions.append(action)
+		for h in d.hunks:
+			
+			for l in h.lines:
+				action = Action(l.content, file_name, l.origin, l.new_lineno, l.old_lineno)
+				list_actions.append(action)
 
 	return list_actions
 
+def calculate_metrics(commits):
+	commits_metrics = {}
+	for commit in commits:
 
-#orig_stdout = sys.stdout
-#f = open('out.txt', 'w')
-#sys.stdout = f
+		if (len(commit.parents)==2):
+			parent1 = commit.parents[0]
+			parent2 = commit.parents[1]
+
+			base = repo.merge_base(parent1.hex, parent2.hex)
+			base_version = repo.get(base)
+			
+			diff_base_final = repo.diff(base_version, commit)
+			diff_base_parent1 = repo.diff(base_version, parent1)
+			diff_base_parent2 = repo.diff(base_version, parent2)
+
+
+			merge_actions = set_actions(diff_base_final)
+			parent1_actions = set_actions(diff_base_parent1)
+			parent2_actions = set_actions(diff_base_parent2)
+			
+			merge_actions_set = actions_to_set(merge_actions)
+			parent1_actions_set = actions_to_set(parent1_actions)
+			parent2_actions_set = actions_to_set(parent2_actions)
+
+			metrics = {}
+
+			parents_actions_set = set(parent1_actions_set).union(parent2_actions_set)
+
+			rework_actions = set(parent1_actions_set).intersection(parent2_actions_set)
+			rework_actions_relative = '{:.0%}'.format(len(rework_actions) / len(parents_actions_set))
+			rework_actions_absolute = len(rework_actions)
+
+			metrics['Parents rework - relative'] = rework_actions_relative
+			metrics['Parents rework - absolute'] = rework_actions_absolute
+
+
+			wasted_actions = parents_actions_set - merge_actions_set
+			wasted_actions_relative = '{:.0%}'.format(len(wasted_actions) / len(parents_actions_set))
+			wasted_actions_absolute = len(wasted_actions)
+
+			metrics['Wasted actions - relative'] = wasted_actions_relative
+			metrics['Wasted actions - absolute'] = wasted_actions_absolute
+
+
+			additional_actions = merge_actions_set - parents_actions_set
+			additional_actions_relative = '{:.0%}'.format(len(additional_actions) / len(merge_actions_set))
+			additional_actions_absolute = len(additional_actions)
+
+			metrics['Additional actions - relative'] = additional_actions_relative
+			metrics['Additional actions - absolute'] = additional_actions_absolute
+
+
+			no_effort = merge_actions_set.intersection(parents_actions_set)
+			no_effort_relative = '{:.0%}'.format(len(no_effort) / len(merge_actions_set))
+			no_effort_absolute = len(no_effort)
+
+
+			metrics['No effort actions - relative'] = no_effort_relative
+			metrics['No effort actions - absolute'] = no_effort_absolute
+
+
+			commits_metrics[commit.hex] = metrics
+
+	return commits_metrics	
+
+
 
 #repo_url = 'git://github.com/tayanemoura/teste_merge.git'
 
@@ -74,76 +124,12 @@ current_working_directory = os.getcwd()
 
 repo_path = current_working_directory + "/" + sys.argv[2]
 
+repo = clone_repository(repo_url, repo_path) 
 
-repo = clone_repository(repo_url, repo_path) #
+commits_metrics = calculate_metrics(repo.walk(repo.head.target, GIT_SORT_TIME | GIT_SORT_REVERSE))
 
-for commit in repo.walk(repo.head.target, GIT_SORT_TIME | GIT_SORT_REVERSE):
-	
-	if (len(commit.parents)==2):
-		print("-------------------------------------------------------------------- \nCommit:")
-		print (commit.hex)
-		print("-------------------------------------------------------------------- \nParents:")
-		parent1 = commit.parents[0]
-		parent2 = commit.parents[1]
+print(commits_metrics)
 
-		print (parent1.hex)
-		print (parent2.hex)
-
-
-		base = repo.merge_base(parent1.hex, parent2.hex)
-		print("-------------------------------------------------------------------- \nMerge base:")
-		print(base)
-
-
-		base_version = repo.get(base)
+shutil.rmtree(repo_path)
 		
-		diff_base_final = repo.diff(base_version, commit)
-		diff_base_parent1 = repo.diff(base_version, parent1)
-		diff_base_parent2 = repo.diff(base_version, parent2)
-
-
-		merge_actions = set_actions(diff_base_final)
-		parent1_actions = set_actions(diff_base_parent1)
-		parent2_actions = set_actions(diff_base_parent2)
-		
-
-		print ("\nmerge actions")
-		merge_actions_set = actions_to_set(merge_actions)
-		print ("parent 1 actions")
-		parent1_actions_set = actions_to_set(parent1_actions)
-		print ("parent 2 actions")
-		parent2_actions_set = actions_to_set(parent2_actions)
-		print()
-
-
-		parents_actions_set = set(parent1_actions_set).union(parent2_actions_set)
-
-		rework_actions = set(parent1_actions_set).intersection(parent2_actions_set)
-		#print ("\nrework actions")
-		#print (rework_actions)
-		#print('Rework actions: {:.0%}'.format(len(rework_actions) / len(parents_actions_set)))
-
-
-		print("--------------------------------------------------------------------\nWasted Effort\n")
-		wasted_actions = parents_actions_set - merge_actions_set
-		print (wasted_actions)
-		print('Wasted effort: {:.0%}'.format(len(wasted_actions) / len(parents_actions_set)))
-
-		additional_actions = merge_actions_set - parents_actions_set
-		print("--------------------------------------------------------------------\nAdditional Effort\n")
-		print('Additional effort: 	{:.0%}'.format(len(additional_actions) / len(merge_actions_set)))
-		print (additional_actions)
-		
-		no_effort = merge_actions_set.intersection(parents_actions_set)
-		print("--------------------------------------------------------------------\n No Effort\n")
-		print(no_effort)
-
-
-		print("-------------------------------------------------------------------- \nEnd - Commit \n")
-			
-
-
-
-#sys.stdout = orig_stdout
-#f.close()			
 
