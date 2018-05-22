678e52ebee511e1d5f5b5a9d242c48a4060dec94
tayanemoura <tayanemoura@id.uff.br>
2018-05-21 22:47:09 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+setuptools.setup(
+	name='merge-effort',    
+	version='0.1',
+	license='MIT',
+	url='https://github.com/gems-uff/merge-effort',
+	author='Tayane Moura and Leonardo Murta',
+	author_email='tayanemoura@id.uff.br',
+	description='a script to measure merge effort',
+	packages= setuptools.find_packages(),
+	entry_points={
+		'console_scripts':[ 'merge-effort = mergeeffort.merge_analysis:main' ]
+	},
+	install_requires=[
+        "pygit2>=0.27.0"
+    ],
+
+	)
